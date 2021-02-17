d402cf1d4463f8981f5975c52f7802860628ea60
Nadno <eunadno@gmail.com>
2021-02-17 19:44:19 -0300

    class app created with basic structure

---
+++ backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+
+class App {
+  public express: express.Application;
+
+  public constructor() {
+    this.express = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  private middlewares(): void {
+    this.express.use(express.json());
+    this.express.use(cors());
+  }
+
+  private routes(): void {}
+}
+
+export default new App().express;
