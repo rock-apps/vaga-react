7d62f57aa5e61d47e4ed8643889aeb4ff34588ee
Nadno <eunadno@gmail.com>
2021-02-19 18:03:07 -0300

    controller, added getPage to product as response

--- backend/src/controllers/product.ts
+++ backend/src/controllers/product.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
 
 import db from '../../products.json';
+import getPage from '../utils/getPage';
 
 class Product {
   public index(req: Request, res: Response) {
-    return res.status(200).json({
-      products: db.products,
-    });
+    const { page, offset } = req.query;
+    return res.status(200).json(getPage(page, offset, db.products));
   }
 
   public categories(req: Request, res: Response) {
