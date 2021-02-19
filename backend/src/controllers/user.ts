cb773c4d62520aa99014f27d51dfc99a88860547
Nadno <eunadno@gmail.com>
2021-02-19 17:05:40 -0300

    controller user, created signIn method

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -4,6 +4,7 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 
 const STATUS_CODE_OK = 200;
+const STATUS_CODE_CREATED = 201;
 const STATUS_CODE_BAD_REQUEST = 400;
 const STATUS_CODE_UNAUTHORIZED = 401;
 const STATUS_CODE_SERVER_ERROR = 500;
@@ -11,17 +12,17 @@ const STATUS_CODE_SERVER_ERROR = 500;
 class User {
   async signUp(req: Request, res: Response) {
     const { name, tel, email, password, avatar, address } = req.body;
-    
+
     const emailAlreadyExists = await db('users')
       .select('users.email')
       .where('users.email', '=', email);
-    
+
     if (emailAlreadyExists.length > 0) {
       return res
         .status(STATUS_CODE_BAD_REQUEST)
         .json({ message: 'Endereço de email já cadastrado!' });
     }
-    
+
     const hash = bcrypt.hashSync(password, 10);
 
     await db('users').insert({
@@ -33,7 +34,27 @@ class User {
       address,
     });
 
-    return res.sendStatus(201);
+    return res.sendStatus(STATUS_CODE_CREATED);
+  }
+
+  async signIn(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const [account] = await db('users')
+      .select('*')
+      .where('users.email', '=', email);
+    
+    if (!account) return res.sendStatus(STATUS_CODE_BAD_REQUEST);
+    
+    const match = bcrypt.compareSync(password, account.password);
+    if (!match) return res.sendStatus(STATUS_CODE_BAD_REQUEST);
+
+    res.status(STATUS_CODE_OK).json({
+      account: {
+        id: account.id,
+        email: account.email,
+      }
+    })
   }
 }
 
