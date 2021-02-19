7a46ceb304d465c6f4754e7eeab53ca82082b87c
Nadno <eunadno@gmail.com>
2021-02-19 00:51:56 -0300

    adjustment on typescript class syntax

--- backend/src/controllers/product.ts
+++ backend/src/controllers/product.ts
@@ -1,20 +1,19 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+
 import db from '../../products.json';
 
 class Product {
-  index(req: Request, res: Response) {
+  public index(req: Request, res: Response) {
     return res.status(200).json({
       products: db.products,
     });
   }
 
-  categories(req: Request, res: Response) {
+  public categories(req: Request, res: Response) {
     return res.status(200).json({
       categories: db.categories,
     });
   }
-
-  create
 }
 
 export default new Product();
