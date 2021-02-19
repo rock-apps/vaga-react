cdd0dc3075b4b2d754824724749695fb809fec4d
Nadno <eunadno@gmail.com>
2021-02-19 17:46:26 -0300

    utils, get pagination

--- backend/src/utils/getPagination.ts
+++ backend/src/utils/getPagination.ts
@@ -1,2 +1,16 @@
 export const isValidQuery = (value: any) =>
   typeof value === 'string' && isFinite(parseInt(value));
+
+export function getPagination(page: any, offset: any) {
+  const itemsForPage = Number(offset);
+  const selectedPage = Number(page);
+
+  const from = Number(selectedPage) * itemsForPage - itemsForPage;
+  const to = Number(selectedPage) * itemsForPage;
+
+  return {
+    itemsForPage,
+    from,
+    to,
+  };
+}
