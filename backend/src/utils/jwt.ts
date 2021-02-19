244721c600f6c76923b280bd3aeae8b0d10e4919
Nadno <eunadno@gmail.com>
2021-02-19 18:12:18 -0300

    util, created generate jwt and refresh jwt

---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+import JWT from 'jsonwebtoken';
+
+const tokenPrivateKey = process.env.JWT_TOKEN_PRIVATE_KEY;
+const refreshTokenPrivateKey = process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY;
+
+const options = { expiresIn: '30 minutes' };
+const refreshOptions = { expiresIn: '30 days' };
+
+export interface Payload {
+  id: number;
+  version?: number;
+}
+
+export const generateJwt = (payload: Payload) =>
+  JWT.sign(payload, tokenPrivateKey, options);
+export const generateRefreshJwt = (payload: Payload) =>
+  JWT.sign(payload, refreshTokenPrivateKey, refreshOptions);
