f555764748b0eb3ed891632c6dbe71129310ef46
Nadno <eunadno@gmail.com>
2021-02-21 07:07:55 -0300

    middleware, created jsonBadRequest

--- backend/src/middlewares/response.ts
+++ backend/src/middlewares/response.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request } from "express";
+import { NextFunction, Request, Response } from "express";
 
 const TYPE_JSON = 'aplication/json';
 const STATUS_CODE_OK = 200;
@@ -7,19 +7,29 @@ const STATUS_CODE_UNAUTHORIZED = 401;
 const STATUS_CODE_NOT_FOUND = 404;
 const STATUS_CODE_SERVER_ERROR = 500;
 
-const jsonOk = function (data: object, message: string, metadata: object) {
-  const status = STATUS_CODE_OK;
-  message = (message) ? message : '';
-  metadata = metadata ? metadata : {};
+type ResponseHandle = (data: object, message: string) => Response;
 
-  this.status(status);
+const jsonOk: ResponseHandle = function (data, message) {
+  message = message || '';
+
+  this.status(STATUS_CODE_OK);
   this.type(TYPE_JSON);
 
-  return this.json({ message, data, metadata, status });
+  return this.json({ message, data, status });
+}
+
+const jsonBadRequest: ResponseHandle = function (data, message) {
+  message = message || '';
+
+  this.status(STATUS_CODE_BAD_REQUEST);
+  this.type(TYPE_JSON);
+
+  return this.json({ message, data, status });
 }
 
 const response = (req: Request, res: any, next: NextFunction) => {
   res.jsonOk = jsonOk;
+  res.jsonBadRequest = jsonBadRequest; 
 
 
   next();
