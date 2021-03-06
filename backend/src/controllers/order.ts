f1db75be930ac715fb985dd2e84c98dd800b5994
Nadno <eunadno@gmail.com>
2021-03-06 12:31:56 -0300

    removed paypal sdk, and create a simple create order method

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,89 +1,14 @@
-require('dotenv').config();
-
 import { Request, Response } from 'express';
-import paypal from '@paypal/checkout-server-sdk';
-import formatProductsToPaypal, {
-  Products,
-  PurchaseUnits,
-} from '../utils/formatProductsToPaypal';
-
-const environment = new paypal.core.SandboxEnvironment(
-  process.env.PAYPAL_CLIENT_ID,
-  process.env.PAYPAL_SECRET_ID
-);
-const client = new paypal.core.PayPalHttpClient(environment);
+import db from '../database/connection';
 
 class Order {
-  public async create(req: Request, res: Response) {
-    const products: Products[] = req.body.products;
-    let items: PurchaseUnits;
-
-    if (!products)
-      return res.status(400).json({ message: 'Conteúdo inválido' });
-
-    try {
-      items = formatProductsToPaypal(products);
-    } catch (err) {
-      res.status(400).json({ message: 'Conteúdo inválido' });
-    }
-
-    const request = new paypal.orders.OrdersCreateRequest();
-    request.requestBody({
-      intent: 'AUTHORIZE',
-      application_context: {
-        return_url: 'https://example.com',
-        cancel_url: 'https://example.com',
-        locale: 'pt-BR',
-      },
-      purchase_units: [{ ...items }],
-    });
-
-    let order;
-    try {
-      order = await client.execute(request);
-    } catch (err) {
-      res.sendStatus(500);
-      throw err;
-    }
-
-    res.status(200).json({
-      token: order.result.id,
-    });
-  }
-
-  public async capture(req: Request, res: Response) {
-    const { orderID, authorizationID } = req.body;
-    if (!orderID) return res.sendStatus(400);
-
-    const request = new paypal.orders.OrdersGetRequest(orderID);
-
-    let order, capture;
-    try {
-      order = await client.execute(request);
-    } catch (err) {
-      console.error(err);
-      res.sendStatus(500);
-      throw err;
-    }
-
-    res.json({
-      order: order.result,
-      capture: capture.result,
-    });
-
-    try {
-      {
-        const request = new paypal.payments.AuthorizationsCaptureRequest(
-          authorizationID
-        );
-        request.requestBody({});
-
-        capture = await client.execute(request);
-        const captureID = capture.result.id;
-      }
-    } catch (err) {
-      console.error(err);
-    }
+  public async create(req: Request, res: Response): Promise<Response> {
+	  const { id, products } = req.body;
+	  
+	  const [account] = await db('users').select('*').where('users.id', '=', id);
+	  if (!account) return res.jsonBadRequest({ message: 'Ocorreu um erro ao criar seu pedido, recarregue a página ou entre mais tarde.' });
+	  
+	  return res.jsonOk({ account, products });
   }
 }
 
