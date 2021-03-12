4866ff8a1c4fb7e7b12993f52405182bcd8821e9
Nadno <eunadno@gmail.com>
2021-03-12 11:54:33 -0300

    create rating method and valid item params/query

--- backend/src/controllers/products/product.ts
+++ backend/src/controllers/products/product.ts
@@ -1,31 +1,58 @@
 import { Request, Response } from 'express';
 
 import ProductFilter from './filter';
-import db from '../../../products.json';
+import JSONdb from '../../../products.json';
 import getPage from '../../utils/getPage';
-
-const ERROR_RESPONSE = { products: [] };
+import db from '../../database/connection';
 
 class Product {
-  public index(req: Request, res: Response) {
+  public index(req: Request, res: Response): Response {
     const { page, offset } = req.query;
-    return res.jsonOk(getPage(page, offset, db.products));
+    return res.jsonOk(getPage(page, offset, JSONdb.products));
   }
 
-  public item(req: Request, res: Response) {
+  public async item(req: Request, res: Response): Promise<Response> {
     const { query, params } = req;
     const { filter } = params;
 
-    if (typeof filter === 'string') {
-      try {
-        const filterHandler = ProductFilter[filter];
-        return res.jsonOk(filterHandler(query.value));
-      } catch (err) {
-        return res.jsonBadRequest(ERROR_RESPONSE);
-      }
-    }
+    if (!('value' in query) || !(filter in ProductFilter))
+      return res.jsonBadRequest({ message: 'Filtro inválido' });
 
-    return res.jsonServerError(ERROR_RESPONSE);
+    try {
+      const filterHandler = ProductFilter[filter];
+      return res.jsonOk(await filterHandler(query.value));
+    } catch (err) {
+      return res.jsonServerError();
+    }
+  }
+
+  public async rating(req: Request, res: Response): Promise<Response> {
+    const { rate, comment, product_id, user_id } = req.body;
+
+    try {
+      const [ratingId] = await db('rate').insert({
+        rate,
+        product_id,
+        user_id,
+        comment,
+      });
+
+      if (!ratingId)
+        return res.jsonBadRequest({
+          message: 'Não foi possível criar um comentário',
+        });
+
+      {
+        const comment = await db('rate')
+          .where('rate.id', '=', ratingId)
+          .join('users', 'rate.user_id', '=', user_id)
+          .select('rate.*', 'users.name', 'users.avatar');
+
+        res.jsonOk({ comment });
+      }
+    } catch {
+      res.jsonServerError();
+    }
   }
 }
 
