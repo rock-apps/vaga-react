b775a81de45387a9cfc2175f240847384db70cbf
Nadno <eunadno@gmail.com>
2021-03-12 12:09:35 -0300

    add cep to users migrate

--- backend/src/database/migrations/00_create_users.ts
+++ backend/src/database/migrations/00_create_users.ts
@@ -1,16 +1,20 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-  return knex.schema.createTable('users', (table) => {
+  return knex.schema.createTable('users', table => {
     table.increments('id').primary();
+
+    table.string('avatar');
+    table.string('name').notNullable();
     table.string('email').notNullable();
     table.string('password').notNullable();
+
+    table.string('tel').notNullable();
+    table.string('cep').notNullable();
     table.string('address').notNullable();
-    table.string('name').notNullable();
-    table.string('tel');
-    table.string('avatar');
+
     table.integer('jwtVersion').defaultTo(0).notNullable();
-  })
+  });
 }
 
 export async function down(knex: Knex) {
