9a034dee13849a6e0beefdd5922acf4da5b26633
Nadno <eunadno@gmail.com>
2021-03-13 15:25:12 -0300

    refactor, return user name, avatar, and id

--- backend/src/utils/login.ts
+++ backend/src/utils/login.ts
@@ -11,16 +11,18 @@ const checkLogin = async ({
   password,
 }: Login) => {
   const [account] = await db('users')
-    .select('*')
+    .select('name', 'avatar', 'id', 'password')
     .where('users.email', '=', email);
 
   const ERROR_MESSAGE = 'Senha ou e-mail incorretos';
   if (!account) throw ERROR_MESSAGE;
+  
+  const { password: dbPassword, ...rest } = account;
 
-  const match = bcrypt.compareSync(password, account.password);
+  const match = bcrypt.compareSync(password, dbPassword);
   if (!match) throw ERROR_MESSAGE;
-
-  return account;
+  
+  return rest;
 };
 
 export default checkLogin;
