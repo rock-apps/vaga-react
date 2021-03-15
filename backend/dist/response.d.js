b2fa70cfbf92529d3f7300c39a8b68d357807c8f
Nadno <eunadno@gmail.com>
2021-03-15 19:52:16 -0300

    remove dist

--- backend/dist/response.d.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-var _express = require("express");
--- backend/dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-var _app = _interopRequireDefault(require("./app"));
-
-_app.default.listen(3333);
